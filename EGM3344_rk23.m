b67125c4f7209fac6b97f7ba436a9a51232549ea
echocreek88-coda <echocreek88@gmail.com>
2025-11-29 22:31:51 -0500

    Add files via upload
    G val updated, ODE func implemented

--- EGM3344_rk23.m
+++ EGM3344_rk23.m
@@ -1,4 +1,44 @@
-function [t, y] = rk23(tspan, y0, dy_dt, tol)
+function dydt = threeBodyODE(t,y)
+% threeBodyODE_simple - explicit 3-body ODE system for use with myODE45
+%
+% y = [x1 y1 z1 vx1 vy1 vz1 x2 y2 z2 vx2 vy2 vz2 x3 y3 z3 vx3 vy3 vz3]'
+
+    G = 6.674e-11;           % gravitational constant (can be scaled)
+    m1=1; m2 = 1;  m3 = 1; %masses
+
+    % Extract positions and velocities
+    x1 = y(1);  y1 = y(2);  z1 = y(3);
+    vx1 = y(4); vy1 = y(5); vz1 = y(6);
+
+    x2 = y(7);  y2 = y(8);  z2 = y(9);
+    vx2 = y(10); vy2 = y(11); vz2 = y(12);
+
+    x3 = y(13); y3 = y(14); z3 = y(15);
+    vx3 = y(16); vy3 = y(17); vz3 = y(18);
+
+    % Compute distance vectors and magnitudes
+    r12 = [x2 - x1; y2 - y1; z2 - z1];
+    r13 = [x3 - x1; y3 - y1; z3 - z1];
+    r23 = [x3 - x2; y3 - y2; z3 - z2];
+
+    d12 = norm(r12+eps^2*zeros(3,1));
+    d13 = norm(r13+eps^2*zeros(3,1));
+    d23 = norm(r23+eps^2*zeros(3,1));
+
+    % Accelerations (Newton’s law of gravitation)
+    a1 = G*(m2*r12/d12^3 + m3*r13/d13^3);
+    a2 = G*(m1*(-r12)/d12^3 + m3*r23/d23^3);
+    a3 = G*(m1*(-r13)/d13^3 + m2*(-r23)/d23^3);
+
+    % Collect derivatives
+    dydt = [...
+        vx1; vy1; vz1; a1(1); a1(2); a1(3); ...
+        vx2; vy2; vz2; a2(1); a2(2); a2(3); ...
+        vx3; vy3; vz3; a3(1); a3(2); a3(3) ];
+end
+
+
+function [t, r1,r2,r3] = rk23(tspan, y0, dy_dt, tol)
 % RK23   Adaptive IVP solver based on embedded RK formulas.
 % Input:
 %   du_dt   defines f in u'(t) = f(t, u) 
@@ -10,7 +50,7 @@ function [t, y] = rk23(tspan, y0, dy_dt, tol)
 %   u       solution values (array, n+1 by m)
 
     if nargin < 3 %default ODE builder
-        y = 0;
+        dy_dt = @threeBodyODE;
         tol = 1e-6;
     elseif nargin < 4
         tol = 1e-6;  % default tolerance
@@ -55,5 +95,19 @@ function [t, y] = rk23(tspan, y0, dy_dt, tol)
         h = min(q*h, tspan(2) - t(i));     % don't step past the end
     end
 
-    y = y.';    % conform to MATLAB output convention
+    r1 = y(1:3, :)';
+    r2 = y(7:9,:)';
+    r3 = y(13:15,:)';    % conform to MATLAB output convention
 end
+%{
+r1 = [2,0,0];
+v1 = [0,1,0];
+r2 = [0,1,0];
+v2 = [0,0,0];
+r3 = [0,0,1];
+v3 = [0,0,0];
+%}
+y0 = [r1, v1, r2, v2, r3, v3];  % pass in vals as row vectors
+tspan = [0 1e3]; %initial and final time value
+
+[t,r1,r2,r3]=rk23(tspan,y0 );
--- EGM3344_rk4.m
+++ EGM3344_rk4.m
@@ -1,4 +1,45 @@
-function [t, y] = rk4(tspan, y0, dy_dt,n)
+function dydt = threeBodyODE(t,y)
+% threeBodyODE_simple - explicit 3-body ODE system for use with myODE45
+%
+% y = [x1 y1 z1 vx1 vy1 vz1 x2 y2 z2 vx2 vy2 vz2 x3 y3 z3 vx3 vy3 vz3]'
+
+    G = 6.674e-11;           % gravitational constant (can be scaled)
+    m1=1; m2 = 1;  m3 = 1; %masses
+
+    % Extract positions and velocities
+    x1 = y(1);  y1 = y(2);  z1 = y(3);
+    vx1 = y(4); vy1 = y(5); vz1 = y(6);
+
+    x2 = y(7);  y2 = y(8);  z2 = y(9);
+    vx2 = y(10); vy2 = y(11); vz2 = y(12);
+
+    x3 = y(13); y3 = y(14); z3 = y(15);
+    vx3 = y(16); vy3 = y(17); vz3 = y(18);
+
+    % Compute distance vectors and magnitudes
+    r12 = [x2 - x1; y2 - y1; z2 - z1];
+    r13 = [x3 - x1; y3 - y1; z3 - z1];
+    r23 = [x3 - x2; y3 - y2; z3 - z2];
+
+    d12 = norm(r12+eps^2*zeros(3,1));
+    d13 = norm(r13+eps^2*zeros(3,1));
+    d23 = norm(r23+eps^2*zeros(3,1));
+
+    % Accelerations (Newton’s law of gravitation)
+    a1 = G*(m2*r12/d12^3 + m3*r13/d13^3);
+    a2 = G*(m1*(-r12)/d12^3 + m3*r23/d23^3);
+    a3 = G*(m1*(-r13)/d13^3 + m2*(-r23)/d23^3);
+
+    % Collect derivatives
+    dydt = [...
+        vx1; vy1; vz1; a1(1); a1(2); a1(3); ...
+        vx2; vy2; vz2; a2(1); a2(2); a2(3); ...
+        vx3; vy3; vz3; a3(1); a3(2); a3(3) ];
+end
+
+
+
+function [t, r1,r2,r3] = rk4(tspan, y0, dy_dt,n)
 % RK4    Fourth-order Runge-Kutta for an IVP.
 % Input:
 %   du_dt   defines f in y'(t) = f(t, u) 
@@ -10,7 +51,7 @@ function [t, y] = rk4(tspan, y0, dy_dt,n)
 %   y       solution values (array, n+1 by m)
     
     if nargin < 3 %default ODE builder
-        dy_dt = 0;
+        dy_dt = @threeBodyODE;
         n = tspan(2)*100;
     elseif nargin < 4
         n = tspan(2)*100; % default steps
@@ -34,9 +75,22 @@ function [t, y] = rk4(tspan, y0, dy_dt,n)
       y(:, i+1) = y(:, i) + (k1 + 2*(k2 + k3) + k4) / 6;
     end
 
-    y = y.';    % conform to MATLAB output convention
+    r1 = y(1:3, :)';
+    r2 = y(7:9,:)';
+    r3 = y(13:15,:)';% conform to MATLAB output convention
 end
+%{
+r1 = [2,0,0];
+v1 = [0,1,0];
+r2 = [0,1,0];
+v2 = [0,0,0];
+r3 = [0,0,1];
+v3 = [0,0,0];
+%}
+y0 = [r1, v1, r2, v2, r3, v3];  % pass in vals as row vectors
+tspan = [0 1e3]; %initial and final time value
 
-f = @(t,y) -y.^2;
+[t,r1,r2,r3]=rk4(tspan,y0 );
 
-[t,y]=rk4([0 30], 10, f);
+%[t1,trucel] = ode45(@threeBodyODE,tspan,y0);
+
